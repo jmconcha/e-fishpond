39702ddd16e54cf21e481e901c37443c75092523
mc <jmconcha.pro@gmail.com>
2025-12-24 00:25:14 +0800

    deleted python virtual environment files

--- raspberrypi/main.py
+++ raspberrypi/main.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials, db
 import time
 
 # Load credentials
-cred = credentials.Certificate("../service_account_key.json")
+cred = credentials.Certificate("./service_account_key.json")
 
 # Initialize app
 firebase_admin.initialize_app(cred, {
@@ -11,45 +11,45 @@ firebase_admin.initialize_app(cred, {
 })
 
 # Define callback functions for real-time updates
-def on_ph_level_update(message):
-    print(f"[pH Level] Updated: {message.data}")
+# def on_ph_level_update(message):
+#     print(f"[pH Level] Updated: {message.data}")
 
-def on_water_temperature_update(message):
-    print(f"[Water Temperature] Updated: {message.data}")
+# def on_water_temperature_update(message):
+#     print(f"[Water Temperature] Updated: {message.data}")
 
-def on_dissolved_oxygen_update(message):
-    print(f"[Dissolved Oxygen] Updated: {message.data}")
+# def on_dissolved_oxygen_update(message):
+    # print(f"[Dissolved Oxygen] Updated: {message.data}")
 
 # Set up real-time listeners for each sensor
-db.reference('/sensors/ph_level').listen(on_ph_level_update)
-db.reference('/sensors/water_temperature').listen(on_water_temperature_update)
-db.reference('/sensors/dissolved_oxygen').listen(on_dissolved_oxygen_update)
+# db.reference('/sensors/ph_level').listen(on_ph_level_update)
+# db.reference('/sensors/water_temperature').listen(on_water_temperature_update)
+# db.reference('/sensors/dissolved_oxygen').listen(on_dissolved_oxygen_update)
 
-print("Listeners started. Waiting for updates...")
+# print("Listeners started. Waiting for updates...")
 
 # Send initial data
-ref = db.reference('/sensors/ph_level')
+ref = db.reference('/sensors/dissolved_oxygen')
 ref.set({
-    'value': 30,
-    'unit': 'pH'
+    'value': 3.0,
+    'unit': 'mg/L'
 })
 
 ref = db.reference('/sensors/water_temperature')
 ref.set({
-    'value': 27.5,
+    'value': 2,
     'unit': '°C'
 })
 
-ref = db.reference('/sensors/dissolved_oxygen')
+ref = db.reference('/sensors/ph_level')
 ref.set({
     'value': 1,
-    'unit': 'mg/L'
+    'unit': 'pH'
 })
 
 # Keep the program running to listen for real-time updates
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    print("\nShutting down...")
-    exit()
+# try:
+#     while True:
+#         time.sleep(1)
+# except KeyboardInterrupt:
+#     print("\nShutting down...")
+#     exit()

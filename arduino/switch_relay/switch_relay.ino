56c52d5a3dcdd0aeeeac3b5512a0fedd56f17dae
Jayson M. Concha <jmconcha.pro@gmail.com>
2026-01-18 13:25:42 +0800

    relay and water temperature arduino code updates

--- arduino/switch_relay/switch_relay.ino
+++ arduino/switch_relay/switch_relay.ino
@@ -1,14 +1,11 @@
 void  setup() {
   pinMode(8, OUTPUT);
   digitalWrite(8, LOW);
-  //digitalWrite(8, HIGH);
-  
 }
 
 void  loop() {
-  delay(3000);
-  digitalWrite(8, HIGH);
-  delay(3000);
-  digitalWrite(8, LOW);
-
+//  delay(3000);
+//  digitalWrite(8, HIGH);
+//  delay(3000);
+//  digitalWrite(8, LOW);
 }

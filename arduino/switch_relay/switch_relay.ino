f9515d62fa9e1164d9cf676d7ff390fcd413c53f
Jayson M. Concha <jmconcha.pro@gmail.com>
2026-02-04 19:36:50 +0800

    arduino ph calibration

--- arduino/switch_relay/switch_relay.ino
+++ arduino/switch_relay/switch_relay.ino
@@ -1,6 +1,12 @@
 void  setup() {
-  pinMode(8, OUTPUT);
-  digitalWrite(8, LOW);
+//  pinMode(8, OUTPUT);
+//  digitalWrite(8, LOW);
+//  pinMode(9, OUTPUT);
+//  digitalWrite(9, LOW);
+//  pinMode(10, OUTPUT);
+//  digitalWrite(10, LOW);
+  pinMode(11, OUTPUT);
+  digitalWrite(11, LOW);
 }
 
 void  loop() {

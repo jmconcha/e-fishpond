59760d723b6fd0de02779dc42685160087a3aa93
Jayson Concha <jmconcha.pro@gmail.com>
2026-02-09 23:19:14 +0800

    local emulators setup

--- mobile-app/firebase.ts
+++ mobile-app/firebase.ts
@@ -1,21 +1,24 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { initializeApp } from 'firebase/app';
+import { getDatabase, connectDatabaseEmulator } from 'firebase/database';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 // import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
-import { initializeAuth } from 'firebase/auth';
+import { initializeAuth, connectAuthEmulator } from 'firebase/auth';
+
 // TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+
+const USE_FIREBASE_EMULATOR =
+  process.env.EXPO_PUBLIC_USE_FIREBASE_EMULATOR === 'true';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DB_URL,
-  projectId: "e-fishpond",
-  storageBucket: "e-fishpond.firebasestorage.app",
-  messagingSenderId: "90603907240",
-  appId: "1:90603907240:web:898f0a981a49af1cf47a9d"
+  projectId: 'e-fishpond',
+  storageBucket: 'e-fishpond.firebasestorage.app',
+  messagingSenderId: '90603907240',
+  appId: '1:90603907240:web:898f0a981a49af1cf47a9d',
 };
 
 // Initialize Firebase
@@ -25,3 +28,20 @@ export const database = getDatabase(app);
 //   persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 // });
 export const auth = initializeAuth(app);
+
+// local firebase, emulator setup
+
+if (USE_FIREBASE_EMULATOR) {
+  console.log('Using Firebase Emulator');
+  connectAuthEmulator(
+    auth,
+    process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN || 'http://192.168.1.23:9099',
+  );
+  connectDatabaseEmulator(
+    database,
+    process.env.EXPO_PUBLIC_HOST || '192.168.1.23',
+    parseInt(process.env.EXPO_PUBLIC_DB_PORT || '9000'),
+  );
+} else {
+  console.log('Using Production Firebase');
+}
